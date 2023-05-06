9e7d47fece11df209a977d1c69d71b1757f133dd
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-06 13:14:43 -0300

    finalizando aula de testes

--- src/templates/Form-Component/index.tsx
+++ src/templates/Form-Component/index.tsx
@@ -2,7 +2,7 @@
 
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import {zodResolver} from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const schema = z.object({
   name: z.string().min(2, 'coloque um nome valido'),
@@ -11,7 +11,13 @@ const schema = z.object({
 
 type FormProps = z.infer<typeof schema>
 
-const Form = () => {
+interface FormComponentProps {
+  handleFormSubmit: (data: FormProps) => void
+}
+
+const Form = ({
+  handleFormSubmit
+}: FormComponentProps) => {
 
   const {
     handleSubmit,
@@ -36,28 +42,37 @@ const Form = () => {
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(handleFormSubmit)}
       className='
         p-4
         bg-black
         flex
         flex-col
         gap-4
+        text-red-500
       '
     >
 
       <h2>Form</h2>
+
+
 
       <input
         {...register('name')}
         placeholder='name'
         aria-label="name"
       />
+      {errors.name && (
+        <p>{errors.name.message}</p>
+      )}
       <input
         {...register('surname')}
         placeholder='surname'
         aria-label="surname"
       />
+      {errors.surname && (
+        <p>{errors.surname.message}</p>
+      )}
 
       <button
         type="submit"
